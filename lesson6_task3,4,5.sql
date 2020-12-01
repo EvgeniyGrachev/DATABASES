7c3f029633b2f02660ba1de5ed52576de19359ad
Evgeniy <zzz12@list.ru>
2020-12-01 13:23:19 +0300

    task_3,4,5

---
+++ lesson6_task3,4,5.sql
@@ -0,0 +1,32 @@
+USE vk;
+
+-- Задание 3. 
+
+SELECT
+	(SELECT gender FROM profiles WHERE user_id = likes.user_id) AS gender,
+	COUNT(*) AS total
+    FROM likes
+    GROUP BY gender
+    ORDER BY total DESC
+    LIMIT 1;  
+
+
+-- Задание 4.
+
+SELECT COUNT(*) FROM likes 
+  WHERE target_type_id = 2
+    AND target_id IN (SELECT * FROM (
+      SELECT user_id FROM profiles ORDER BY birthday DESC LIMIT 10
+    ) AS sorted_profiles ) 
+;
+  
+ -- Задание 5. 
+   
+SELECT 
+  CONCAT(first_name, ' ', last_name) AS user, 
+	(SELECT COUNT(*) FROM likes WHERE likes.user_id = users.id) + 
+	(SELECT COUNT(*) FROM media WHERE media.user_id = users.id) + 
+	(SELECT COUNT(*) FROM messages WHERE messages.from_user_id = users.id) AS overall_activity 
+	  FROM users
+	  ORDER BY overall_activity
+	  LIMIT 10;
